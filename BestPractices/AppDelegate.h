34db09b4392b6f3df44d576f8c52fc26bafd382e
Can Berk Güder <cbg@cbg.me>
2014-03-27 21:52:42 -0400

    Initial commit

---
+++ BestPractices/AppDelegate.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic) UIWindow *window;
+
+@end
---
+++ README.md
@@ -0,0 +1,4 @@
+BestPractices
+=============
+
+A collection of things I consider to be best practices in test-driven iOS development
